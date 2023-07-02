63861c65ab33c07d5b18af81e1e2e7b714e93420
Tricster <mediosrity@gmail.com>
2023-07-03 00:54:30 +0800

    set wrap
    Signed-off-by: Tricster <mediosrity@gmail.com>

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -87,6 +87,7 @@ local plug_map = {
 
 	-- Plugin suda.vim
 	["n|<A-s>"] = map_cu("SudaWrite"):with_silent():with_noremap():with_desc("editn: Save file using sudo"),
+	["n|<leader>q"] = map_cmd(":Ex"):with_noremap():with_desc("Return to files"),
 }
 
 bind.nvim_load_mapping(plug_map)
